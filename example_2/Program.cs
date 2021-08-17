13a67c7ff6dcd3902845f869532082def7462027
arkadii <arkadii.classsic@gmail.com>
2021-08-17 10:54:32 +0300

    changes_examples

--- example_2/Program.cs
+++ example_2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassMessage;
+using System;
 
 namespace example_2
 {
@@ -16,6 +17,28 @@ namespace example_2
             д) ***Создать метод, который производит частотный анализ текста.
                 В качестве параметра в него передается массив слов и текст, в качестве результата метод возвращает
                 сколько раз каждое из слов массива входит в этот текст. Здесь требуется использовать класс Dictionary.*/
+            Console.WriteLine("Вас приветствует программа демонстрации возможностей статического класса Message.");
+
+            Console.WriteLine("\nИмеется следующий текст: \n" + Message.text);
+
+            Console.WriteLine("\nВыведем слова текста, которые содержат не более 5 букв:");
+            Message.GetWordsByLength(5);
+
+            Console.WriteLine();
+            Console.Write("\nУдалим из текста слова, заканчивающиеся на 'я': ");
+            Message.DeleteWordByEndChar('я');
+
+            Console.WriteLine();
+            Console.WriteLine("\nСамое длинное слово в тексте: " + Message.FindMaxLengthWord());
+
+
+            Console.WriteLine("\nСформированная строка StringBuilder из самых длинных слов сообщения: \n" + Message.GetLongWordsString());
+
+            Console.WriteLine("\nПроизведём частотный анализ текста: ");
+            string[] arr = { "шел", "его", "тело", "жив", "он" };
+            Message.FrequencyAnalysis(arr, Message.text);
+
+            Console.ReadKey();
         }
     }
 }
